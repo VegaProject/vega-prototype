502ed313e84295edc59bf489a6f987e1b3ed8e08
Georgek <georgek00012@gmail.com>
2017-03-24 09:41:08 -0700

    fixed open-zeppelin file loader

--- contracts/Vega.sol
+++ contracts/Vega.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.4.8;
 
-import "../installed_contracts/zepplelin/contracts/token/StandardToken.sol";
+import "../installed_contracts/zeppelin/contracts/token/StandardToken.sol";
 import './OutgoingMigrationTokenInterface.sol';
 import './IncomingMigrationTokenInterface.sol';
 
