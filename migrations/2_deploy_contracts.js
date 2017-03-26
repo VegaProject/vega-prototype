ff98c2c13647ba197f9c3b2cf0f3c6e9508cb59a
Georgek <georgek00012@gmail.com>
2017-03-26 12:04:04 -0700

    messing with migrations

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,12 @@
-var ConvertLib = artifacts.require("./ConvertLib.sol");
-var MetaCoin = artifacts.require("./MetaCoin.sol");
+var VegaToken = artifacts.require("./VegaToken.sol");
+var Project = artifacts.require("./Project.sol");
+var IncomingMigrationTokenInterface = artifacts.require("./IncomingMigrationTokenInterface.sol");
+var OutgoinMigrationTokenInterface = artifacts.require("./OutgoinMigrationTokenInterface.sol");
+
 
 module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin);
+  deployer.deploy(VegaToken);
+  deployer.deploy(Project);
+  deployer.deploy(IncomingMigrationTokenInterface);
+  deployer.deploy(OutgoinMigrationTokenInterface);
 };
