2f0c0b00c98bc12de6a0041b08838c1a47a7101e
Georgek <georgek00012@gmail.com>
2017-03-31 19:25:12 -0700

    migration update

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,8 @@
 var VegaToken = artifacts.require("./VegaToken.sol");
 var Project = artifacts.require("./Project.sol");
-var Liquidate = artifacts.require("./Liquidate");
+var Liquidate = artifacts.require("./Liquidate.sol");
+var ProxyContract = artifacts.require("./Proxy.sol");
+var FundOffering = artifacts.require("./FundOffering.sol");
 var IncomingMigrationTokenInterface = artifacts.require("./IncomingMigrationTokenInterface.sol");
 var OutgoingMigrationTokenInterface = artifacts.require("./OutgoingMigrationTokenInterface.sol");
 
@@ -8,6 +10,8 @@ module.exports = function(deployer) {
   deployer.deploy(VegaToken);
   deployer.deploy(Project);
   deployer.deploy(Liquidate);
+  deployer.deploy(ProxyContract);
+  deployer.deploy(FundOffering);
   deployer.deploy(IncomingMigrationTokenInterface);
   deployer.deploy(OutgoingMigrationTokenInterface);
 };
