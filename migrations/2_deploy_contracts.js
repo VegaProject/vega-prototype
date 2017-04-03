bd4a626718db1941e4b163d1f8025433cddc04dc
root <georgek00012@gmail.com>
2017-04-03 21:16:27 +0000

    remove old migrations

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,17 +1,11 @@
 var VegaToken = artifacts.require("./VegaToken.sol");
-var Project = artifacts.require("./Project.sol");
-var Liquidate = artifacts.require("./Liquidate.sol");
-var ProxyContract = artifacts.require("./Proxy.sol");
-var FundOffering = artifacts.require("./FundOffering.sol");
+var Club = artifacts.require("./Club.sol");
 var IncomingMigrationTokenInterface = artifacts.require("./IncomingMigrationTokenInterface.sol");
 var OutgoingMigrationTokenInterface = artifacts.require("./OutgoingMigrationTokenInterface.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(VegaToken);
-  deployer.deploy(Project);
-  deployer.deploy(Liquidate);
-  deployer.deploy(ProxyContract);
-  deployer.deploy(FundOffering);
+  deployer.deploy(Club);
   deployer.deploy(IncomingMigrationTokenInterface);
   deployer.deploy(OutgoingMigrationTokenInterface);
 };
