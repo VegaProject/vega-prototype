49d18a4031475ec35e13134ba86d63ee9c32a83b
Georgek <georgek00012@gmail.com>
2017-04-13 18:16:59 -0700

    truffle config stuff

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,13 +1,13 @@
 var VegaToken = artifacts.require("./VegaToken.sol");
 var Club = artifacts.require("./Club.sol");
-//var EtherDeltaRef = artifacts.require("./EtherDeltaRef.sol");
+var EtherDelta = artifacts.require("./EtherDelta.sol");
 var IncomingMigrationTokenInterface = artifacts.require("./IncomingMigrationTokenInterface.sol");
 var OutgoingMigrationTokenInterface = artifacts.require("./OutgoingMigrationTokenInterface.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(VegaToken);
   deployer.deploy(Club);
-  //deployer.deploy(EtherDeltaRef);
+  deployer.deploy(EtherDelta);
   deployer.deploy(IncomingMigrationTokenInterface);
   deployer.deploy(OutgoingMigrationTokenInterface);
 };
