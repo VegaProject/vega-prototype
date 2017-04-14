67b137da3c864d4fa13637f1375145275906b332
Georgek <georgek00012@gmail.com>
2017-04-13 17:57:38 -0700

    truffle build back working

--- truffle.js
+++ truffle.js
@@ -2,11 +2,23 @@
 require('babel-register')
 
 module.exports = {
+  build: "webpack",
   networks: {
     development: {
-      host: 'localhost',
+      host: "localhost",
       port: 8545,
-      network_id: '*' // Match any network id
+      network_id: "*" // match any network
+    },
+    live: {
+      host: "178.25.19.88", // Random IP for example purposes (do not use)
+      port: 80,
+      network_id: 1,        // Ethereum public network
+      // optional config values:
+      // gas
+      // gasPrice
+      // from - default address to use for any transaction Truffle makes during migrations
+      // provider - web3 provider instance Truffle should use to talk to the Ethereum network.
+      //          - if specified, host and port are ignored.
     }
   }
 }
