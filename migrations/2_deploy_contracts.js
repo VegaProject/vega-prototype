f704b7b8003fb18009c68886567752e5e5a2bf8e
Georgek <georgek00012@gmail.com>
2017-04-13 17:20:05 -0700

    ,

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,11 +1,13 @@
 var VegaToken = artifacts.require("./VegaToken.sol");
 var Club = artifacts.require("./Club.sol");
+//var EtherDeltaRef = artifacts.require("./EtherDeltaRef.sol");
 var IncomingMigrationTokenInterface = artifacts.require("./IncomingMigrationTokenInterface.sol");
 var OutgoingMigrationTokenInterface = artifacts.require("./OutgoingMigrationTokenInterface.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(VegaToken);
   deployer.deploy(Club);
+  //deployer.deploy(EtherDeltaRef);
   deployer.deploy(IncomingMigrationTokenInterface);
   deployer.deploy(OutgoingMigrationTokenInterface);
 };
