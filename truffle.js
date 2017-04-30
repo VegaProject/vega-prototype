e3be4eae0d3860dbe98c6ec5d7e9f46011d6986d
Georgek <georgek00012@gmail.com>
2017-04-29 23:17:46 -0700

    added ropsten network config

--- truffle.js
+++ truffle.js
@@ -9,6 +9,11 @@ module.exports = {
       port: 8545,
       network_id: "*" // match any network
     },
+    ropsten: {
+      host: "13.84.180.240",
+      port: 30303,
+      network_id: 3
+    },
     live: {
       host: "178.25.19.88", // Random IP for example purposes (do not use)
       port: 80,
