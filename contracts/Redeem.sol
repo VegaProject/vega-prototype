224014a8369d93f97f3693764ef6703cbd1176c9
Georgek <georgek00012@gmail.com>
2017-05-17 16:10:26 -0700

    Redeem depreachated

--- contracts/Redeem.sol
+++ contracts/Redeem.sol
@@ -1,4 +1,11 @@
 pragma solidity ^0.4.8;
+
+/**
+*
+*  REDEEM IS DEPREACHATED, NEW CONTRACT WILL BE AVAILABLE SOON.
+*
+*/
+
 import './deps/Ownable.sol';
 import './deps/Helpers.sol';
 import './VegaToken.sol';
@@ -32,7 +39,7 @@ contract Redeem is Ownable, Helpers, TokenTracker {
   Project public Prj;
 
   mapping(address => uint) public currentBalance;
-
+  mapping(address => mapping (address => uint)) remaining;
 
   /// @param _vegaTokenAddr VegaToken contract address.
   function Redeem(VegaToken _vegaTokenAddr, Project _projectAddr) {
@@ -45,10 +52,10 @@ contract Redeem is Ownable, Helpers, TokenTracker {
     Prj = Project(_projectAddr);
   }
 
-  function getProportionOfCurrentTotalSupply(address _who) public constant returns (uint) {
+  function getProportionOfCurrentTotalSupply(address _who, uint _value) public constant returns (uint) {
     uint amount = VT.balanceOf(_who);
     uint totalAmount = VT.totalSupply();
-    uint proportion = converter(amount, amount, totalAmount);
+    uint proportion = converter(_value, amount, totalAmount);
     return proportion;
   }
 
@@ -57,12 +64,22 @@ contract Redeem is Ownable, Helpers, TokenTracker {
     return amount;
   }
 
-  function getBalanceForEachToken() public constant returns (bool) {
+  function getBalanceForEachToken() public returns (bool) {
     for(uint i = 0; i < getAddressCount(); ++i) {
       uint amount = getBalanceOfToken(getAddressAtIndex(i));
       currentBalance[getAddressAtIndex(i)] = amount;
    }
    return true;
+  }
+
+  function redeemTokens(address _token, uint _value) public returns (bool) {
+    getBalanceForEachToken(); // updates to the current balance of the fund.
+    uint amount = getBalanceOfToken(_token);    // get the balance of the token that is being redeemed
+    uint claim = getProportionOfCurrentTotalSupply(msg.sender, amount); // getting the amount of tokens the Vega holder is entitled to redeem.
+    if(claim < _value) throw;                                           // throw if user is trying to redeem more than they are entitled to.
+    ERC20(_token).transferFrom(Prj, msg.sender, _value);                // call the transferFrom function to move the coins from the Project Offer contract to the msg.sender, with the token value.
+    remaining[msg.sender][_token] -= _value;
+    return true;                                                        // return true
   }
 
   // next function will get the amount of tokens for each token address and convert it to the amount of tokens that a Vega holder is entitled to, by using the getProportionOfCurrentTotalSupply.
